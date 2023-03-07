86ea292d6a8c012ed7055f36c4f753500670e2f0
Nikita\Nick <kineeetik@gmail.com>
2023-03-07 23:26:26 +0300

    Added docs for `ECS` autoload

--- addons/ECS/src/ECS.gd
+++ addons/ECS/src/ECS.gd
@@ -1,15 +1,29 @@
 extends Node
 
+
+## Autoload to made all ecs things work[br][br]
+## Handles [i]registered[/i] entities and filters[br][br]
+## Provides methods to activate and use [i]discrete mode[/i][br]
+## [i]discrete mode[/i] allows you to process systems step-by-step
+
+
+
 const COMPONENT_TYPE_PROPERTY = "COMPONENT_TYPE"
 const COMPONENT_READONLY_META_NAME = "READONLY_COMPONENT"
 
+## Emmited after ECS entered discrete mode
 signal entered_discrete_mode
+## Emmited after ECS exited discrete mode
 signal exited_discrete_mode
+## Only in discrete mode[br]
+## Emmited after next system processed
 signal process_pushed
 
-
+## Array of all registered entities 
 var entities : Array[Entity] = []
+## Array of all registered filters
 var filters : Array[EntityFilter] = []
+## If true, discrete mode is active
 var in_discrete_mode : bool = false
 
 var _systems_queue : Array[System] = []
@@ -20,6 +34,7 @@ func _enter_tree():
 	get_tree().node_added.connect(_on_node_added)
 
 
+## Allow [code]filter[/code] to get valid registered entities
 func register_filter(filter:EntityFilter):
 	if filter.registered:
 		return
@@ -34,6 +49,7 @@ func register_filter(filter:EntityFilter):
 	filter.was_registered.emit()
 
 
+## [code]filter[/code] stops to get valid entities and clears [code]valid_entities[/code]
 func unregister_filter(filter:EntityFilter):
 	if not filter.registered:
 		return
@@ -45,6 +61,7 @@ func unregister_filter(filter:EntityFilter):
 	filter.registered = false
 
 
+## Allow [code]entity[/code] being passed into registered filters
 func register_entity(entity:Entity):
 	if entity.inside_ecs:
 		return
@@ -57,6 +74,7 @@ func register_entity(entity:Entity):
 	revise_entity(entity)
 
 
+## [code]entitity[/code] stops being passed into filters
 func unregister_entity(entity:Entity):
 	if not entity.inside_ecs:
 		return
@@ -71,11 +89,15 @@ func unregister_entity(entity:Entity):
 	entity.inside_ecs = false
 
 
+## Frees all regsitered entities
 func clear_entities():
 	for ent in entities.duplicate():
 		ent.free()
 
 
+## Usually called when [code]entity[/code] changed it's components set[br]
+## Adds [code]entity[/code] into filters it fits[br]
+## Removes an [code]entity[/code] from filters it doesn't fit, if [code]entity[/code] in it
 func revise_entity(entity:Entity):
 	if not entity.inside_ecs:
 		return
@@ -89,6 +111,8 @@ func revise_entity(entity:Entity):
 				filter.remove_entity(entity)
 
 
+## Only in discrete mode[br]
+## Processes next system
 func push_update():
 	if not _has_systems_in_queue():
 		push_warning("Has no systems to push update")
@@ -99,6 +123,8 @@ func push_update():
 	process_pushed.emit()
 
 
+## Only in discrete mode[br]
+## Processes systems before a system which is processed first in one frame in queue
 func complete_systems_cycle():
 	if not in_discrete_mode:
 		push_warning("Attempted complete systems cycle when discrete mode isn't active")
@@ -113,6 +139,8 @@ func complete_systems_cycle():
 		process_pushed.emit()
 
 
+## Activates step-by-step system processing[br]
+## Shows discrete mode control panel
 func enter_discrete_mode():
 	if in_discrete_mode:
 		push_warning("Attempted to enter discrete mode when it's active")
@@ -133,6 +161,8 @@ func enter_discrete_mode():
 	entered_discrete_mode.emit()
 
 
+## Diactivates step-by-step system processing[br]
+## Hides discrete mode control panel
 func exit_discrete_mode():
 	await get_tree().process_frame
 	
@@ -148,10 +178,12 @@ func exit_discrete_mode():
 	exited_discrete_mode.emit()
 
 
+## True if [code]instance[/code] can be interpreted as component
 func is_instance_component(instance:Object):
 	return COMPONENT_TYPE_PROPERTY in instance
 
 
+## True if [code]instance[/code] can be interpreted as component and was set as readonly
 func is_component_readonly(instance:Object):
 	if not is_instance_component(instance):
 		return false
@@ -159,6 +191,8 @@ func is_component_readonly(instance:Object):
 	return instance.has_meta(COMPONENT_READONLY_META_NAME)
 
 
+## Sets component as readonly[br]
+## Creates instance's meta with name specified in [code]COMPONENT_READONLY_META_NAME[/code]
 func set_component_readonly(instance:Object):
 	if not is_instance_component(instance):
 		return false
