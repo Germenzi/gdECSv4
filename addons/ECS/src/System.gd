15fad0cc7be8de2701f7e7d26f72233123fd7a7a
Nikita\Nick <kineeetik@gmail.com>
2023-03-08 15:10:17 +0300

    Added docs for `System` class

--- addons/ECS/src/System.gd
+++ addons/ECS/src/System.gd
@@ -2,6 +2,16 @@ extends Node
 
 class_name System
 
+## System implementation as [Node][br]
+## You can activate and diactivate it using [code]active[/code] exported variable.[br]
+## Define entities processing behaviour in [code]on_entity_process(entity:Entity, delta:float)[/code]
+## virtual method.[br]
+## Specify necessary and banned components as a return value of [code]get_necessary_components[/code] 
+## and [code]get_banned_components[/code] methods.
+
+## If system active, it processes entities in the normal way.[br]
+## If system deactivated ([code]active = false[/code]), it processes no entities until
+## will be activated.
 @export
 var active : bool = true:
 	set(v):
@@ -9,7 +19,12 @@ var active : bool = true:
 		set_process(active)
 
 
+## Filter to get valid entities.[br]
+## Usually no reason to change.
 var entity_filter : EntityFilter
+## Systems in discrete mode doesn't process entities automatically.[br]
+## Call [code]push_process()[/code] to process, but usually there is no reason
+## to do it. [code]ECS[/code] manage discrete mode by itself.
 var in_discrete_mode : bool = false
 
 var _allow_push : bool = false
@@ -44,7 +59,10 @@ func _process(delta:float):
 		_allow_push = false
 
 
-func push_process():
+## Only in discrete mode.[br]
+## Usually there is no reason to call it manually.[br]
+## Makes the system processing entities
+func push_process() -> void:
 	if in_discrete_mode:
 		_allow_push = true
 
@@ -54,14 +72,19 @@ func _process_entities(delta:float):
 		on_entity_process(i, delta)
 
 
-# virtual
-func on_entity_process(entity:Entity, delta:float):
+## Vurtual[br][br]
+## Define in this method behaviour of processing entity
+## [code]delta[/code] is the same as [code]delta[/code] in [method Node._process] method
+func on_entity_process(entity:Entity, delta:float) -> void:
 	pass
 
-# virtual
-func get_necessary_components() -> Array:
+## Virtual[br][br]
+## Override this to return an array of necessary components' names
+func get_necessary_components() -> Array[String]:
 	return []
 
-# virtual
-func get_banned_components() -> Array:
+
+## Virtual[br][br]
+## Override this to return an array of banned components' names
+func get_banned_components() -> Array[String]:
 	return []
