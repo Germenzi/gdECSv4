e0d773c5b17a2148c25344cf305d53f24757ecf5
Nikita\Nick <kineeetik@gmail.com>
2023-03-16 21:53:38 +0300

    Added discrete panel singleton to plugin

--- addons/ECS/plugin.gd
+++ addons/ECS/plugin.gd
@@ -4,12 +4,17 @@ extends EditorPlugin
 const ECS_SIGNLETON_PATH = "res://addons/ECS/src/ECS.gd"
 const ECS_SINGLETON_NAME = "ECS"
 
+const PANEL_SINGLETON_PATH = "res://addons/ECS/DiscreteModeMonitor/DiscreteMonitor.tscn"
+const PANEL_SINGLETON_NAME = "DiscreteMonitor"
+
 func _enter_tree():
 	add_autoload_singleton(ECS_SINGLETON_NAME, ECS_SIGNLETON_PATH)
+	add_autoload_singleton(PANEL_SINGLETON_NAME, PANEL_SINGLETON_PATH)
 
 
 func _exit_tree():
 	remove_autoload_singleton(ECS_SINGLETON_NAME)
+	remove_autoload_singleton(PANEL_SINGLETON_NAME)
 
 
 func get_plugin_icon():
